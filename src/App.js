e228c00d9e44be4d721b682fd7a09de919829cc4
wuxingsheng <wuxingsheng@qutoutiao.net>
2019-08-04 21:02:15 +0800

    add ant

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
  import Header from './common/header/index'
  import store from './store/index'
  import { Provider } from 'react-redux'
-
+ import 'antd/dist/antd.css';
+ 
+ import { Button , Alert } from 'antd';
 function App() {
   return (
     <Provider store={store}>
        <Header></Header>
+       <Button  type="primary" >btn</Button>
+       <Alert message="Success Text" type="success" />
     </Provider>
 
   );
