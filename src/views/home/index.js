45719c423638ae0ea479ada99b62a76be7dd7e05
wuxingsheng <wuxingsheng@qutoutiao.net>
2019-08-06 11:06:23 +0800

    components add done

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,11 +1,30 @@
 import React , { Component} from 'react';
 import Header from '../../common/header/index'
+import {HomeWraper,HomeLeft,HomeRight} from './style'
+
+import List from './components/List'
+import Recomend from './components/Recomend'
+
 class Details extends Component{
     render(){
-        return <div>
-                    <Header></Header>
-                        home
-                </div> 
+        return (
+            <div>
+                <Header></Header>
+                <HomeWraper>
+                   <HomeLeft>
+                        <List></List>
+                   </HomeLeft>
+                   <HomeRight>
+                        <Recomend></Recomend>
+                   </HomeRight>
+
+                         
+                </HomeWraper> 
+            </div>
+        )
+        
+        
+       
     }
 }
 
---
+++ src/views/home/style.js
@@ -0,0 +1,15 @@
+import styled  from 'styled-components';
+
+export const HomeWraper = styled.div`
+    width:960px;
+    display:flex;
+    margin:0 auto;
+`
+
+export const HomeLeft = styled.div`
+    width:625px;
+`
+
+export const HomeRight = styled.div`
+    width:245px;
+`
