29b8d377ea2b49ad3fbeaac5c91e1b23e62eff69
wuxingsheng <wuxingsheng@qutoutiao.net>
2019-08-08 19:27:56 +0800

    components add done

--- src/views/home/store/actionCreater.js
+++ src/views/home/store/actionCreater.js
@@ -5,17 +5,14 @@ import {CHANGE_ATICLE_LIST} from './constants'
 
 import {getArticleList as getData} from '../../../api/api'
 
- 
- 
 export const getList = ()=>{
   return (dispatch)=>{
     getData().then((data)=>{
-       
       const action = {
         type:CHANGE_ATICLE_LIST,
         value:data || []
       }
-      return  dispatch(action);
+      dispatch(action);
     });
      
   }
