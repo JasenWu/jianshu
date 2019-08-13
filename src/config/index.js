c03622d47bde8f6be18114467059466b13817dcb
wuxingsheng <wuxingsheng@qutoutiao.net>
2019-08-13 23:18:27 +0800

    login in modules

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 //export const API_DOMAIN = 'http://rd-yapi.qutoutiao.net';
-export const API_DOMAIN = 'http://localhost:8081';
+export const API_DOMAIN = 'http://127.0.0.1:7001';
 export const API_CODE = {
   SUCCESS:0,
 
